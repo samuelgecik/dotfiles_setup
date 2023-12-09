85da71d56db55776214a269b75a7d5afa77eb1e8
Samuel Gecik <68155940+samuelgecik@users.noreply.github.com>
2023-12-09 15:37:34 +0100

    Ask for the user's repository url

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 setup_dotfiles() {
-    git clone https://github.com/samuelgecik/dotfiles.git ~/.dotfiles
-    cd ~/.dotfiles
-    ./install
+    # Ask the user for the URL of their dotfiles repository
+    echo "Please enter the URL of your dotfiles repository:"
+    read DOTFILES_REPO_URL
+
+    # Validate if the URL is not empty
+    if [ -z "$DOTFILES_REPO_URL" ]; then
+        echo "No URL entered. Dotfiles setup skipped."
+        return
+    fi
+
+    # Clone the dotfiles repository
+    git clone "$DOTFILES_REPO_URL" ~/.dotfiles
+
+    # Check if the repository was cloned successfully
+    if [ -d "~/dotfiles" ]; then
+        # Navigate to the dotfiles directory and run the installation script
+        cd ~/.dotfiles
+        if [ -f "./install" ]; then
+            ./install_dotfiles.sh
+        else
+            echo "No install_dotfiles.sh script found in the repository."
+        fi
+    else
+        echo "Failed to clone the repository. Check the URL and try again."
+    fi
 }
 
 setup_dotfiles
+ 
